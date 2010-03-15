5c76025d1ccdf60b28c2492ae8954a812158643f
scztt <scott@artificia.org>
2010-03-15 11:57:53 -0700

    don't track intermediate build files!

--- Intermediate Build Files/SuperColliderAU.build/Script-D5F6AB340EF42BC4002DDA8C.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p $BUILD_DIR/SuperColliderAU.component/Contents/Resources/plugins
-cp  $BUILD_DIR/plugins/*.scx $BUILD_DIR/SuperColliderAU.component/Contents/Resources/plugins
-mkdir -p $BUILD_DIR/SuperColliderAU.component/Contents/Resources/synthdefs
